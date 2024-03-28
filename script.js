9d5038f6c7f3f2674292c94aee431f9af8ce594c
ajpisani <156853937+ajpisani@users.noreply.github.com>
2024-03-27 23:20:55 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,76 +1,119 @@
-// heres a line that moves up and down the screen
-
-// creating the canvas
+let numsx = 1;
+let numrx = 1;
+let numtx = 1;
+let numux = 1;
+let numvx = 1;
+let numwx = 1;
+let numxx = 1;
+let numyx = 1;
+let numzx = 1;
+let nums = 1;
+let numr = 1;
+let numt = 1;
+let numu = 1;
+let numv = 1;
+let numw = 1;
+let numx = 1;
+let numy = 1;
+let numz = 1;
+let numa = 40;
+let numb = 40;
+let numc = 20;
+let numd = 21;
+let numEL = 1;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
   frameRate(30);
 }
 
-// initialization of xnum and ynum (pooploop was juts for figuring shit out)
-
-let xnum = 1;
-let ynum = 1;
-let zanum = 1;
-let zbnum = 1;
-let zcnum = 1;
-let xxnum = 1;
-let xynum = 1;
-
-pooploop = true;
-
-// function for the line
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
 function draw() {
-  background(110);
+  background(0);
+  stroke(numxx, numyx, numzx);
+  fill(numsx, numyx, numxx);
+  ellipse(nums, numy, numa, numb);
 
-  // the function to make the line move back and forth!
+  stroke(numyx, numzx, numxx);
+  fill(numtx, numvx, numxx);
+  ellipse(nums, numz, numb, numa);
 
-  xnum = xnum += ynum;
+  stroke(numyx, numzx, numwx);
+  fill(numux, numux, numyx);
+  ellipse(numv, numx, numc, numd);
 
-  if (xnum == 600) {
-    ynum = -1;
-  }
-  if (xnum == 0) {
-    ynum = 1;
-  }
+  stroke(numwx, numzx, numxx);
+  fill(numzx, numwx, numyx);
+  ellipse(numt, numw, numd, numc);
 
-  xxnum = Math.floor(Math.random() * 100) + 95;
-  xynum = Math.floor(Math.random() * 4) + 1;
+  stroke(numwx, numyx, numxx);
+  fill(numyx, numzx, numwx);
+  arc(numx, numy, numa, numb, numEL, HALF_PI + QUARTER_PI);
 
-  // the function that changes the color
+  stroke(numwx, numyx, numxx);
+  fill(numvx, numzx, numwx);
+  arc(numy, numw, numb, numc, numEL, PI + PI);
 
-  // to tell you if the loop is running or not (again, obsolete now that it works)
-
-  console.log(pooploop);
-
-  //outer color / color
-
-  stroke(zanum, zbnum, zcnum);
-  fill(zcnum, zbnum, zanum);
-
-  // the line positon (staring x, starting y, ending x, ending y)
-
-  ellipse(xnum, xnum, 20 + xynum, 30 + xynum);
+  stroke(nums, numz, numw);
+  noFill();
+  rect(numv, numx, nums, numt);
 }
-
-// mouse / pressing functions
-
-function mousePressed() {
-  pooploop = false;
-  noLoop();
-}
-
-function mouseReleased() {
-  pooploop = true;
-  loop();
-}
-
-// random color on keychange function
 
 function keyPressed() {
-  zanum = Math.floor(Math.random() * 255) + 1;
-  zbnum = Math.floor(Math.random() * 255) + 1;
-  zcnum = Math.floor(Math.random() * 255) + 1;
-  console.log(zanum, zbnum, zcnum);
+  nums = Math.floor(Math.random() * windowHeight) + 1;
+  numt = Math.floor(Math.random() * windowHeight) + 1;
+  numu = Math.floor(Math.random() * windowHeight) + 1;
+  numv = Math.floor(Math.random() * windowHeight) + 1;
+  numw = Math.floor(Math.random() * windowWidth) + 1;
+  numx = Math.floor(Math.random() * windowWidth) + 1;
+  numy = Math.floor(Math.random() * windowWidth) + 1;
+  numz = Math.floor(Math.random() * windowWidth) + 1;
+
+  numsx = Math.floor(Math.random() * 255) + 1;
+  numtx = Math.floor(Math.random() * 255) + 1;
+  numux = Math.floor(Math.random() * 255) + 1;
+  numvx = Math.floor(Math.random() * 255) + 1;
+  numwx = Math.floor(Math.random() * 255) + 1;
+  numxx = Math.floor(Math.random() * 255) + 1;
+  numyx = Math.floor(Math.random() * 255) + 1;
+  numzx = Math.floor(Math.random() * 255) + 1;
+
+  numa = Math.floor(Math.random() * 80) + 40;
+  numb = Math.floor(Math.random() * 90) + 30;
+  numc = Math.floor(Math.random() * 160) + 10;
+  numd = Math.floor(Math.random() * 180) + 5;
+  numEL = Math.floor(Math.random() * 10) + 1;
+
+  console.log(numx, numy, numz, numa, numb);
+}
+
+function touchStartded() {
+  nums = Math.floor(Math.random() * windowHeight) + 1;
+  numt = Math.floor(Math.random() * windowHeight) + 1;
+  numu = Math.floor(Math.random() * windowHeight) + 1;
+  numv = Math.floor(Math.random() * windowHeight) + 1;
+  numw = Math.floor(Math.random() * windowWidth) + 1;
+  numx = Math.floor(Math.random() * windowWidth) + 1;
+  numy = Math.floor(Math.random() * windowWidth) + 1;
+  numz = Math.floor(Math.random() * windowWidth) + 1;
+
+  numsx = Math.floor(Math.random() * 255) + 1;
+  numtx = Math.floor(Math.random() * 255) + 1;
+  numux = Math.floor(Math.random() * 255) + 1;
+  numvx = Math.floor(Math.random() * 255) + 1;
+  numwx = Math.floor(Math.random() * 255) + 1;
+  numxx = Math.floor(Math.random() * 255) + 1;
+  numyx = Math.floor(Math.random() * 255) + 1;
+  numzx = Math.floor(Math.random() * 255) + 1;
+
+  numa = Math.floor(Math.random() * 80) + 40;
+  numb = Math.floor(Math.random() * 90) + 30;
+  numc = Math.floor(Math.random() * 160) + 10;
+  numd = Math.floor(Math.random() * 180) + 5;
+  numEL = Math.floor(Math.random() * 10) + 1;
+
+  console.log(numx, numy, numz, numa, numb);
 }
