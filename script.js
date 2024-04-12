cee64a31c65e110c3eaa7d2109d3c689e54967a8
ajpisani <156853937+ajpisani@users.noreply.github.com>
2024-04-12 12:45:12 -0400

    script.js

--- script.js
+++ script.js
@@ -1,74 +1,61 @@
+//initialize color values
+
+colorCtrlNum = -1;
+colorNum = 1;
+
+//set up a canvas
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(40);
-  colorMode(HSB, 255);
-
-  x = width / 2;
-  y = height / 2;
+  colorMode(HSB);
+  background(0, 0, 0);
+  noLoop();
 }
+
+//resize canvas  when window is resized
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
-dnum = 8;
-cnum = 255;
-bnum = 255;
+
+// draw function
 
 function draw() {
-  background(0, 0, 0);
-  cnum = cnum -= 6;
-  bnum = bnum -= 5;
-  dnum = dnum += 0.2 * dnum;
-  y = y += 3;
+  colorNum += 1;
 
-  let xM = 1;
-
-  for (i = 0; i < 14; i++) {
-    stroke(100, 0, bnum);
-    fill(cnum, 255, bnum);
-
-    //56 ^ AJ;2u7wqa5u6Q733475bv2            we67   <-- a kitten did that
-
-    ellipse(
-      x + dnum * (i / 18) + xM * i,
-      y + dnum * (i / 25) + xM * i,
-      random(12, 18),
-      random(12, 18)
-    );
-    ellipse(
-      x - dnum * (i / 18) + xM * i,
-      y + dnum * (i / 25) + xM * i,
-      random(12, 18),
-      random(12, 18)
-    );
-    ellipse(
-      x - dnum * (i / 18) + xM * i,
-      y - dnum * (i / 25) + xM * i,
-      random(12, 18),
-      random(12, 18)
-    );
-    ellipse(
-      x + dnum * (i / 18) + xM * i,
-      y - dnum * (i / 25) + xM * i,
-      random(12, 18),
-      random(12, 18)
-    );
-
-    xM = xM += i / 20;
+  if (colorNum == 300) {
+    colorNum = 1;
   }
+
+  makeParticles();
 }
+
+makeParticles = function () {
+  fill(colorNum, random(250, 255), 255, random(0.4, 0.8));
+  circle(mouseX + random(-3, 3), mouseY + random(-3, 3), random(10, 20));
+};
 
 function mousePressed() {
-  dnum = 4;
-  bnum = 255;
-  cnum = 255;
-  x = mouseX;
-  y = mouseY;
+  loop();
 }
+function mouseReleased() {
+  noLoop();
+}
+
+function keyPressed() {
+  background(0, 0, 0);
+  colorNum = 0;
+}
+
 function touchStarted() {
-  dnum = 4;
-  bnum = 255;
-  cnum = 255;
-  x = mouseX;
-  y = mouseY;
+  loop();
+}
+
+function touchMoved() {
+  loop();
+}
+
+function touchEnded() {
+  noLoop();
 }
