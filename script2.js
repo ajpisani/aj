bcb0c9cdd7d5def7649964aaeb341d077b070f1f
ajpisani <156853937+ajpisani@users.noreply.github.com>
2024-04-13 11:40:33 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,71 +1,93 @@
-colorNum = 1
+colorNum = 1;
+let mp = false;
 
 let poo = [];
 
-function setup(){
-    createCanvas(window.innerWidth, window.innerHeight)
-    frameRate(40)
-    colorMode(HSB)
-    background(0,0,0)
-    noStroke()
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  frameRate(40);
+  colorMode(HSB);
+  background(0, 0, 0);
+  noStroke();
 }
 
-function windowResized(){
-   resizeCanvas(window.innerWidth, window.innerHeight)
+function windowResized() {
+  resizeCanvas(window.innerWidth, window.innerHeight);
 }
 
-class Particles{
-constructor(){
+class Particles {
+  constructor() {
+    this.x = mouseX;
+    this.y = mouseY;
+    this.xSpeed = random(-2, 2);
+    this.ySpeed = random(-4, -6);
+    this.alpha = 1;
+    this.lifespan = 255;
+    this.velocity = random(1);
+  }
 
-    this.x = window.innerWidth/2
-    this.y = window.innerHeight - (window.innerHeight-20)
-    this.xSpeed = random(-2,2)
-    this.ySpeed = random(-4,-6)
-    this.alpha = 1
-    this.lifespan = 255
-    this.velocity = random(1)
-}
-
-death(){
+  death() {
     return this.lifespan < 0;
+  }
+
+  showParticles() {
+    fill(colorNum, 255, 255, this.alpha);
+    ellipse(this.x, this.y, random(15, 18), random(15, 18));
+  }
+
+  updateParticles() {
+    this.x -= this.xSpeed;
+    this.y -= this.ySpeed + this.velocity;
+    this.alpha -= 0.025;
+    this.lifespan -= 1;
+  }
+
+  updateVelocity() {
+    this.velocity -= 0.2;
+  }
 }
 
-showParticles() {
-    fill(colorNum, 255, 255, this.alpha)
-    ellipse(this.x, this.y, random(15,18), random(15,18))
-}
-
-updateParticles(){
-    this.x -= this.xSpeed
-    this.y -= (this.ySpeed + this.velocity)
-    this.alpha -= (.00002*window.innerHeight)
-    this.lifespan -= 1
-}
-
-updateVelocity(){
-    this.velocity -= .2
-}
-
-}
+runParticles = function () {
+  pee = new Particles();
+  poo.push(pee);
+};
 
 function draw() {
-    background(0,0,0)
-    colorNum = colorNum += 1
+  background(0, 0, 0);
 
-    if (colorNum==315){
-        colorNum=0
+  if (mp == true) {
+    pee = new Particles();
+    poo.push(pee);
+
+    colorNum = colorNum += 1;
+
+    if (colorNum == 315) {
+      colorNum = 0;
     }
+  }
 
-pee = new Particles()
-poo.push(pee)
-
-for (i=0;i<poo.length; i++)
-{
-poo[i].updateVelocity()
-poo[i].showParticles()
-poo[i].updateParticles()
-if (poo[i].death()) { poo.splice(i, 1)}
+  for (i = 0; i < poo.length; i++) {
+    poo[i].updateVelocity();
+    poo[i].showParticles();
+    poo[i].updateParticles();
+    if (poo[i].death()) {
+      poo.splice(i, 1);
+    }
+  }
 }
 
+function mousePressed() {
+  mp = true;
+}
 
+function mouseReleased() {
+  mp = false;
+}
+
+function touchStarted() {
+  mp = true;
+}
+
+function touchEnded() {
+  mp = false;
 }
